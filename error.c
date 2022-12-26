14583c453c7e0ac1a8e8438976bf0abb5e089dfb
admansar <115159995+admansar@users.noreply.github.com>
2022-12-26 07:21:58 -0900

    Add files via upload

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: admansar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 18:05:17 by admansar          #+#    #+#             */
-/*   Updated: 2022/12/26 16:48:05 by admansar         ###   ########.fr       */
+/*   Updated: 2022/12/26 17:21:13 by admansar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,15 @@ int	yup(int j, int n, int i, char *c)
 
 void error_(char **a)
 {
+	int i;
+
+	i = 0;
 	ft_printf("Error\n");
+	while (a[i])
+	{
+		free(a[i]);
+		i++;
+	}
 	free(a);
 	exit(1);
 }
@@ -71,13 +79,13 @@ void	ft_error(char **c)
 		ne = ft_atoi(c[i]);
 		if (check_if_number(c[i], k, p, ne) == 0)
 		{
-			error_(a);
+			error_(c);
 		}
 		else
 			i++;
 	}
 	if (detective(c) == 1 || sensor(c) == 0)
 	{
-			error_(a);
+		error_(c);
 	}
 }
